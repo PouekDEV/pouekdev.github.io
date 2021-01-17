240fa113b933eefd09c9a16b6b715f2391a06432
Jakub Starostecki <64737924+PouekDEV@users.noreply.github.com>
2021-01-17 19:37:40 +0100

    Delete Time Tickle directory

--- Time Tickle/js/loop.js
+++
@@ -1,6 +0,0 @@
-function gameLoop(that){
-    let diff = (Date.now() - that.player.lastUpdate)/1000
-    that.player.time += that.player.hourglass[0].amount * that.player.hourglass[0].multi * diff
-
-    that.player.lastUpdate = Date.now()
-}
