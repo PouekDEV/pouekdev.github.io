06890cf2aa5dc44ca3e9faf6c75aeb8038ca1d41
Jakub Starostecki <jakub.starostecki27@gmail.com>
2021-11-02 20:25:17 +0100

    news haha

---
+++ js/news.js
@@ -0,0 +1,28 @@
+proxyurl = //"https://cors-anywhere.herokuapp.com/"
+console.log("News initiated")
+fetch(proxyurl + "https://pouekdev-news-api.pouekdev.repl.co")
+.then(res => res.json()).then(body => {
+    if(!body) {
+        console.log("Error");
+        document.getElementById("newsbox").style.display = "none";
+    }
+    else{
+        news = body.news;
+        setnewnews();
+    }
+})
+news = []
+var amount = 0;
+function setnewnews(){
+    amount = news.length
+    console.log(news[4])
+    while(amount > 0){
+        amount -= 1;
+        console.log(amount)
+        var tag = document.createElement("span");
+        var text = document.createTextNode(news[amount]);
+        tag.appendChild(text);
+        var element = document.getElementById("news");
+        element.appendChild(tag);
+    }
+}
